1a1ff03ae7997977c6352f3eb01ce703afa7db1d
regit66 <marcindzioch@gmail.com>
2017-01-20 01:07:11 +0100

    doc + code fix

--- violetproduct-swing/src/main/java/com/horstmann/violet/application/autosave/AutoSave.java
+++ violetproduct-swing/src/main/java/com/horstmann/violet/application/autosave/AutoSave.java
@@ -7,7 +7,6 @@ import java.io.File;
 import java.io.IOException;
 import java.io.InputStream;
 
-
 import javax.swing.*;
 import com.horstmann.violet.application.gui.MainFrame;
 import com.horstmann.violet.framework.file.GraphFile;
@@ -29,6 +28,7 @@ import com.horstmann.violet.workspace.Workspace;
  * @author Pawel Majka
  */
 
+
 public class AutoSave implements ActionListener {
 
 	@ResourceBundleBean(key="dialog.autosave.title")
@@ -47,6 +47,10 @@ public class AutoSave implements ActionListener {
 	private final int saveInterval = 60 * second;
 	private final String autoSaveDirectory = System.getProperty("user.home") + File.separator + "VioletUML";
 
+	/**
+	 * Constructor Autosave
+	 *  @param mainFrame where is attached this menu
+	 */
 	public AutoSave(MainFrame mainFrame) {
 		BeanInjector.getInjector().inject(this);
 
@@ -59,6 +63,10 @@ public class AutoSave implements ActionListener {
 		}
 	}
 
+	/**
+	 *  Create Violet directory
+	 * @return true if path was created
+	 */
 	private boolean createVioletDirectory() {
 		File directory = new File(autoSaveDirectory);
 		return directory.isDirectory() || directory.mkdir();
@@ -66,6 +74,9 @@ public class AutoSave implements ActionListener {
 
 	}
 
+	/**
+	 *  Get autosave file in direcotry
+	 */
 	private void openAutoSaveDirectory() {
 		File directory = new File(autoSaveDirectory);
 		emptyFileRemove();
@@ -75,10 +86,14 @@ public class AutoSave implements ActionListener {
 				return;
 
 			createSaveRecoverFrame();
+			AutoSaveRecover aa= new AutoSaveRecover(mainFrame);// not work yet
 
 		}
 	}
 
+	/**
+	 *  Remove Violet empty saves
+	 */
 	private void emptyFileRemove() {
 		File directory = new File(autoSaveDirectory);
 
@@ -91,58 +106,19 @@ public class AutoSave implements ActionListener {
 		}
 	}
 
-
+	/**
+	 * Initialize timer
+	 */
 	private void initializeTimer() {
 		saveTimer = new Timer(saveInterval, (ActionListener) this);
 		saveTimer.setInitialDelay(0);
 		saveTimer.start();
 	}
 
-
-	private void createSaveRecoverFrame() {
-
-		ResourceBundleInjector.getInjector().inject(this);
-		ManiocFramework.BeanInjector.getInjector().inject(this);
-		final JFrame autoSaveFrame = new JFrame(dialogTitle);
-		autoSaveFrame.setDefaultCloseOperation(JFrame.HIDE_ON_CLOSE);
-		Dimension dim = Toolkit.getDefaultToolkit().getScreenSize();
-		autoSaveFrame.setSize(500, 300);
-		autoSaveFrame.setLocation(dim.width / 2 - autoSaveFrame.getSize().width / 2, dim.height / 2 - autoSaveFrame.getSize().height / 2);
-
-		JPanel jPanel = new JPanel();
-		jPanel.setLayout(new GridBagLayout());
-		autoSaveFrame.add(jPanel);
-
-		JButton LoadAutoBtn = new JButton(buttonRecovery);
-		LoadAutoBtn.setSize(100, 50);
-		LoadAutoBtn.addActionListener(new ActionListener() {
-			public void actionPerformed(ActionEvent event)
-			{
-
-				loadAutoSaveFile();
-
-				autoSaveFrame.dispose();
-			}
-		});
-		JButton StartNewBtn = new JButton(buttonNew);
-		LoadAutoBtn.setSize(100, 50);
-		StartNewBtn.addActionListener(new ActionListener() {
-			public void actionPerformed(ActionEvent event) {
-
-			removeAutoSaveFile();
-					autoSaveFrame.dispose();
-
-			}
-		});
-		jPanel.add(LoadAutoBtn);
-		jPanel.add(StartNewBtn);
-		autoSaveFrame.setResizable(false);
-		autoSaveFrame.setAlwaysOnTop(true);
-		autoSaveFrame.setVisible(true);
-	}
-
-
-	private void loadAutoSaveFile() {
+	/**
+	 * Load autosave file
+	 */
+	public void loadAutoSaveFile() {
 		File directory = new File(autoSaveDirectory);
 
 			File[] files = directory.listFiles();
@@ -176,6 +152,55 @@ public class AutoSave implements ActionListener {
 			}
 		}
 	}
+
+	/**
+	 * Open autosave frame
+	 */
+	private void createSaveRecoverFrame() {
+
+		ResourceBundleInjector.getInjector().inject(this);
+		ManiocFramework.BeanInjector.getInjector().inject(this);
+		final JFrame autoSaveFrame = new JFrame(dialogTitle);
+		autoSaveFrame.setDefaultCloseOperation(JFrame.HIDE_ON_CLOSE);
+		Dimension dim = Toolkit.getDefaultToolkit().getScreenSize();
+		autoSaveFrame.setSize(500, 300);
+		autoSaveFrame.setLocation(dim.width / 2 - autoSaveFrame.getSize().width / 2, dim.height / 2 - autoSaveFrame.getSize().height / 2);
+
+		JPanel jPanel = new JPanel();
+		jPanel.setLayout(new GridBagLayout());
+		autoSaveFrame.add(jPanel);
+
+		JButton LoadAutoBtn = new JButton(buttonRecovery);
+		LoadAutoBtn.setSize(100, 50);
+		LoadAutoBtn.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent event)
+			{
+
+				loadAutoSaveFile();
+
+				autoSaveFrame.dispose();
+			}
+		});
+		JButton StartNewBtn = new JButton(buttonNew);
+		LoadAutoBtn.setSize(100, 50);
+		StartNewBtn.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent event) {
+
+				removeAutoSaveFile();
+				autoSaveFrame.dispose();
+
+			}
+		});
+		jPanel.add(LoadAutoBtn);
+		jPanel.add(StartNewBtn);
+		autoSaveFrame.setResizable(false);
+		autoSaveFrame.setAlwaysOnTop(true);
+		autoSaveFrame.setVisible(true);
+	}
+
+	/**
+	 * Remove autosave file
+	 */
 	private void removeAutoSaveFile()
 	{
 		File directory = new File(autoSaveDirectory);
@@ -186,6 +211,10 @@ public class AutoSave implements ActionListener {
 		}
 	}
 
+	/**
+	 * Action Performed
+	 *  @param e event
+	 */
 	@Override
 	public void actionPerformed(ActionEvent e) {
 		for (IWorkspace workspace : mainFrame.getWorkspaceList()) {
