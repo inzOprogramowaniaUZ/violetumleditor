44ec29caee1ea9921885ea5c176d1cc650eb085f
regit66 <marcindzioch@gmail.com>
2017-02-16 22:36:25 +0100

    reformat

--- violetproduct-swing/src/main/java/com/horstmann/violet/application/autosave/IAutoSave.java
+++ violetproduct-swing/src/main/java/com/horstmann/violet/application/autosave/IAutoSave.java
@@ -1,10 +1,5 @@
 package com.horstmann.violet.application.autosave;
 
-/**
- * AutoSave Listener
- * 
- * @author Marta Mrugalska
- */
 public interface IAutoSave {
 	public void reloadSettings();
 }
